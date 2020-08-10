ee80578d2f730251829f2e1bd870f6ae4671ca3d
asxisto <asxisto@gmail.com>
2020-08-10 14:03:48 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,43 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="container">
+          <nav className="navbar">
+            <img src="./../images/ironhack-logo.svg" alt="ironhack logo" />
+            <img src="./../images/menu-top.svg" alt="ironhack logo" />
+          </nav>
+
+          <div className="heroMessage">
+            <h1>Say hello to ReactJS</h1>
+            <p>You will learn a Frontend framework from scratch and become a Ninja Developer</p>
+            <a className="btn-primary" href="#">
+              Awesome!
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img src="./../images/icon1.png" alt="declarative logo" />
+            <h4>Declarative</h4>
+            <p>React makes it painless to create interactive UIs.</p>
+          </div>
+          <div className="col">
+            <img src="./../images/icon2.png" alt="components logo" />
+            <h4>Components</h4>
+            <p>Build encapsulated components that manage their state.</p>
+          </div>
+          <div className="col">
+            <img src="./../images/icon3.png" alt="single-way logo" />
+            <h4>Single-Way</h4>
+            <p>A set og immutable values are passed to the components.</p>
+          </div>
+          <div className="col">
+            <img src="./../images/icon4.png" alt="jsx logo" />
+            <h4>JSX</h4>
+            <p>Statically-typed designed to run on modern browsers.</p>
+          </div>
+        </div>
       </div>
     );
   }
